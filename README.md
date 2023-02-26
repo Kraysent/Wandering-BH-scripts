7031e7a6613e4decc8521e961ed614dfe49d17bd
Artyom Zaporozhets <kraysent@gmail.com>
2023-02-26 22:25:24 +0300

    added README and help messages

---
+++ README.md
@@ -0,0 +1,13 @@
+### models_resolution
+
+Following command will run all of the models with different resolutions and show two figures with distance and bound mass plots.
+
+```shell
+python3 main.py models-resolution
+```
+
+For flags description use 
+
+```shell
+python3 main.py models-resolution --help
+```
--- main.py
+++ main.py
@@ -8,9 +8,9 @@ def cli():
 
 
 @cli.command()
-@click.option("-s", "--save", type=bool)
-@click.option("-st", "--save-trajectories", type=bool)
-@click.option("-c", "--cached", type=bool)
+@click.option("-s", "--save", is_flag=True, type=bool, help="Save to PDF file or just show figures?")
+@click.option("-st", "--save-trajectories", is_flag=True, type=bool, help="Save trajectories to separate file?")
+@click.option("-c", "--cached", is_flag=True, type=bool, help="Already have trajectories on the files? This option will plot them. Ignores -st option but does not ignore -s one.")
 def models_resolution(save, save_trajectories, cached):
     if not cached:
         model(save_trajectories, save)
